8a4dd0b8869a85dbb81e9cb00f8e1f8d61581774
Moxvallix <moxvallix@gmail.com>
2022-05-18 15:49:23 +0930

    feat(admin): adds back to main app button

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_191444) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_01_08_191444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
